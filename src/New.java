5f9ce49ad0cb048cf62512f832f262d5cf4a9ed2
raulchick <szadiraka@mail.ru>
2015-08-18 01:00:39 +0300

    mm

--- src/New.java
+++ src/New.java
@@ -3,7 +3,7 @@
  */
 public class New {
     public static void main(String[]args){
-        System.out.println("test");
+        System.out.println("test ");
         System.out.println("test");
 
     }
