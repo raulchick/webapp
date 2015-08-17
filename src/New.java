2ab793cef2dbda362702f4f2c1de14d1a5d8df0c
raulchick <szadiraka@mail.ru>
2015-08-18 00:58:19 +0300

    mm

---
+++ src/New.java
@@ -0,0 +1,10 @@
+/**
+ * Created by roman on 18.08.15.
+ */
+public class New {
+    public static void main(String[]args){
+        System.out.println("test");
+        System.out.println("test");
+
+    }
+}
